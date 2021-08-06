8fb696c54beabbf9c2646a5a74999e3ac133f66c
AmulyaReddy <amulyareddy05@gmail.com>
2021-08-05 22:51:13 -0400

    Added FIles

--- src/test/java/sample/pages/AmazonPages.java
+++ src/test/java/sample/pages/AmazonPages.java
@@ -2,15 +2,14 @@ package sample.pages;
 
 import org.openqa.selenium.WebDriver;
 
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import org.openqa.selenium.support.PageFactory;
 
 import sample.utility.ReusableUtility;
 
 public class AmazonPages {
-
 	public WebDriver driver1;
 	ReusableUtility RU;
 
